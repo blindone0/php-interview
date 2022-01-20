0340893585e1a07d0785f2afcc2d6e9197fc275c
Igor Radetskiy <mrsettle1@gmail.com>
2022-01-21 00:50:13 +0200

    Here is the php-interview question-answer;

--- Main.php
+++ Main.php
@@ -1,5 +1,13 @@
 <?php
 
+function display($var) {
+    foreach($GLOBALS as $demo => $value) {
+        if ($value === $var) {
+            return $demo;
+        }
+    }
+    return false;
+}
 
 $input = [
     ['parent_id' => 1, 'child_id' => 1],
@@ -21,13 +29,14 @@ foreach ($input as $key => $value) {
     $output+= [$i => array_values($value)];
     $i++;
 }
-
+print "$".display($output)." = [ \n";
 foreach ($output as $key => $value) {
     $outString="$key => [";
     $length = count($value);
     $j=0;
     foreach($value as $v) {
         $j++;
+        print "\t";
         if($length == $j) {
             $outString=$outString.$v;
         } else {
@@ -37,5 +46,6 @@ foreach ($output as $key => $value) {
     $outString = $outString."]";
     echo $outString."\n";
 }
+print "]; \n";
 
 //var_dump($output);
